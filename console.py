851ae2ac517bf58175ee7148d9afb964d21f8bf2
setalxswe <setalxswe@gmail.com>
2023-11-13 04:04:53 -0800

    __console_update__

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ from models import storage
 import json
 import shlex
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,7 +21,12 @@ class HBNBCommand(cmd.Cmd):
     promt = '(hbnb)'
     class_list = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
             }
 
     def do_quit(self, arg):
